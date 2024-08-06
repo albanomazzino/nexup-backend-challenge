ba543e73c7fd5ac368bbb000bdace30d8891b725
albanomazzino <albimazzino@gmail.com>
2024-08-06 10:38:57 -0300

    Relacion: CadenaSupermercados, Supermercado

--- src/com/gestionSupermercados/cadenaSupermercados/CadenaSupermercados.kt
+++ src/com/gestionSupermercados/cadenaSupermercados/CadenaSupermercados.kt
@@ -1,6 +1,9 @@
 package com.gestionSupermercados.cadenaSupermercados
 
+import com.gestionSupermercados.supermercado.Supermercado
+
 data class CadenaSupermercados (
     val nombre : String,
-    val id : Long
+    val id : Long,
+    val supermercados : MutableList<Supermercado>
 )
